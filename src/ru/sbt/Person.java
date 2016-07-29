b8a6b9d054ed832b0b421a5b1a7e3545cc4153c2
keyoh <kdiakov@yandex.ru>
2016-07-29 17:33:51 +0300

    1

--- src/ru/sbt/Person.java
+++ src/ru/sbt/Person.java
@@ -1,53 +1,50 @@
 package ru.sbt;
 
-
 public class Person {
-
-    private final boolean man;
 
     private final String name;
 
+    private final boolean men;
+
     private Person spouse;
 
-
-
-    public Person(boolean man, String name) {
-
-        this.man = man;
-
+    public Person(String name, boolean man) {
         this.name = name;
-
+        this.men = man;
     }
 
-
-
-    /**
-
-     * This method checks gender of persons. If genders are not equal - tries to marry.
-
-     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
-
-     * @param person - new husband/wife for this person.
-
-     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
-
-     */
 
     public boolean marry(Person person) {
-        
+        if (men == person.isMen() || this.spouse == person || person == null) {
+            return false;
+        } else {
+            if (spouse != null) {
+                divorce();
+            }
+            this.spouse = person;
+            person.marry(this);
+            return true;
+        }
     }
 
-
-
-    /**
-
-     * Sets spouse = null if spouse is not null
-
-     * @return true - if person status has been changed
-
-     */
 
     public boolean divorce() {
-
+        if (spouse == null) {
+            return false;
+        } else {
+            Person exSpouse = spouse;
+            spouse = null;
+            exSpouse.divorce();
+            return true;
+        }
     }
+
+    public boolean isMen() {
+        return men;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
